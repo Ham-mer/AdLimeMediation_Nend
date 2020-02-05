f3fbb80fc7ef47b47d72535aed1f87a65f196bbf
Ham-mer <2419568879@qq.com>
2020-02-05 10:48:58 +0800

    .

---
+++ AdLimeMediation_Nend.framework/Headers/AdLimeMediation_Nend.h
@@ -0,0 +1,19 @@
+//
+//  AdLimeMediation_Nend.h
+//  AdLimeMediation_Nend
+//
+//  Created by AdLimeSdk on 2019/6/27.
+//  Copyright © 2019年 AdLimeSdk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AdLimeMediation_Nend.
+FOUNDATION_EXPORT double AdLimeMediation_NendVersionNumber;
+
+//! Project version string for AdLimeMediation_Nend.
+FOUNDATION_EXPORT const unsigned char AdLimeMediation_NendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdLimeMediation_Nend/PublicHeader.h>
+
+#import <AdLimeMediation_Nend/AdLimeNendAdMode.h>
---
+++ AdLimeMediation_Nend.framework/Headers/AdLimeNendAdMode.h
@@ -0,0 +1,18 @@
+//
+//  AdLimeNendAdMode.h
+//  AdLimeMediation_Nend
+//
+//  Created by AdLimeSdk on 2020/2/3.
+//  Copyright © 2020 AdLimeSdk. All rights reserved.
+//
+
+typedef NS_ENUM(NSUInteger, AdLimeNendNativeMode) {
+    ADLIME_NEND_NATIVE_NORMAL = 0,
+    ADLIME_NEND_NATIVE_VIDEO = 1
+};
+
+typedef NS_ENUM(NSUInteger, AdLimeNendInterstitialMode) {
+    ADLIME_NEND_INTERSTITIAL_NORMAL = 0,
+    ADLIME_NEND_INTERSTITIAL_VIDEO = 1,
+    ADLIME_NEND_INTERSTITIAL_FULLSCREEN = 2
+};
